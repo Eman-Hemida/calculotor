73d76f40e6d9feca20bc54d6fab17bc9128707f8
eman hemida <hemidaeaman75@gmail.com>
2024-12-13 14:31:08 +0200

    first commit

---
+++ main.js
@@ -0,0 +1,34 @@
+ var Number1=Number(prompt('Enter First number '))
+ var Number2=Number(prompt('Enter Second number '))
+ var add
+ add=Number1+Number2
+ document.write(add);
+// **************************************************
+
+ var Number1=Number(prompt('Enter First number '));
+var Number2=Number(prompt('Enter Second number '));
+ var sub
+ sub=Number1-Number2
+ document.write("sub");
+// *******************************************************
+
+ var Number1=Number(prompt('Enter First number '));
+var Number2=Number(prompt('Enter Second number '));
+var mult
+mult=Number1*Number2
+document.write(mult);
+// ******************************************
+
+ var Number1=Number(prompt('Enter First number '));
+ var Number2=Number(prompt('Enter Second number '));
+ var dev
+dev=Number1/Number2
+ document.write(dev);
+// *******************************************************
+ var Number1=Number(prompt('Enter First number '));
+ var Number2=Number(prompt('Enter Second number '));
+ var moduls
+ moduls=Number1 % Number2
+ document.write(moduls);
+//**********************************************************
+
